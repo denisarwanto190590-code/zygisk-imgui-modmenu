8d03b3f09dcd14cc90d3eb27b676fbbc2625c6fb
denisarwanto190590-code <denisarwanto190590@gmail.com>
2026-06-30 20:32:31 +0700

    Update hack.h

--- module/src/main/cpp/hack.h
+++ module/src/main/cpp/hack.h
@@ -1,10 +1,10 @@
 #ifndef ZYGISK_IMGUI_MODMENU_HACK_H
 #define ZYGISK_IMGUI_MODMENU_HACK_H
 
+// Perbaikan makro Hook agar sesuai dengan pemanggilan fungsi di hack.cpp
 #define HOOKAF(ret, func, ...) \
     ret (*orig##func)(__VA_ARGS__); \
-    ret my##func(__VA_ARGS__)
-
+    ret func(__VA_ARGS__)
 
 void hack_prepare(const char *game_data_dir);
 
